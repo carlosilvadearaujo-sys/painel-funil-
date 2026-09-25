c7c16d9cb0d2f356e239f8424df1c16623b6b7b5
carlosilvadearaujo-sys <carlosilvadearaujo@gmail.com>
2026-09-25 17:24:33 -0300

    Adicionar suporte PWA offline

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='funil-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/style.css','/app.js','/manifest.json']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
